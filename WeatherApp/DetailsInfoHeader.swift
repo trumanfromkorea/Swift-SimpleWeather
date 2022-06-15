b7fa16e90d60d2799696412ad574c32234558d54
trumanfromkorea <jangjh971008@naver.com>
2022-06-15 12:50:27 +0900

    test: detailsCollectionView header 삽입 테스트

---
+++ WeatherApp/DetailsInfoHeader.swift
@@ -0,0 +1,12 @@
+//
+//  DetailsInfoHeader.swift
+//  WeatherApp
+//
+//  Created by 장재훈 on 2022/06/15.
+//
+
+import UIKit
+
+//class DetailsInfoHeader: UICollectionReusableView {
+//    static let identifier = "DetailsInfoHeader"
+//}
--- WeatherApp/DetailsViewController.swift
+++ WeatherApp/DetailsViewController.swift
@@ -12,15 +12,10 @@ class DetailsViewController: UIViewController {
     static let storyboard = "Main"
 
     var weatherInfo: WeatherModel?
-    
-    let sampleList = [1,2,3,4,5,6]
+
+    let sampleList = [1, 2, 3, 4, 5, 6]
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    @IBOutlet var koreanNameLabel: UILabel!
-    @IBOutlet var iconImageView: UIImageView!
-    @IBOutlet var englishNameLabel: UILabel!
-    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var collectionView: UICollectionView!
 
     @IBOutlet var minTempLabel: UILabel!
     @IBOutlet var maxTempLabel: UILabel!
@@ -33,7 +28,7 @@ class DetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         navigationItem.largeTitleDisplayMode = .never
         configureCollectionView()
         configureOutlets()
@@ -41,10 +36,6 @@ class DetailsViewController: UIViewController {
 
     private func configureOutlets() {
         if let weatherInfo = weatherInfo {
-            koreanNameLabel.text = CityModel.cities.first { $0.id == weatherInfo.id }?.koreanName
-            englishNameLabel.text = weatherInfo.cityName
-            descriptionLabel.text = weatherInfo.weather.first!.description
-
             minTempLabel.text = WeatherModel.generateTemp(weatherInfo.main.minTemp)
             maxTempLabel.text = WeatherModel.generateTemp(weatherInfo.main.maxTemp)
 
@@ -64,20 +55,37 @@ extension DetailsViewController {
     }
 
     private func configureCollectionView() {
-
         // ui
 //        collectionView.backgroundColor = .white.withAlphaComponent(0.7)
 //        collectionView.layer.cornerRadius = 15
         collectionView.showsVerticalScrollIndicator = false
 
         // dataSource
-        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, _ in
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DetailsInfoCell.identifier, for: indexPath) as? DetailsInfoCell else {
                 return nil
             }
 
             return cell
         })
+
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            if kind == UICollectionView.elementKindSectionHeader {
+                guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailsInfoHeader.identifier, for: indexPath) as? DetailsInfoHeader else {
+                    return UICollectionReusableView()
+                }
+
+                header.configure(
+                    CityModel.cities.first { $0.id == self.weatherInfo!.id }!.koreanName,
+                    self.weatherInfo!.cityName,
+                    self.weatherInfo!.weather.first!.description
+                )
+
+                return header
+            } else {
+                return UICollectionReusableView()
+            }
+        }
 
         // snapshot
         var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
@@ -88,7 +96,7 @@ extension DetailsViewController {
         // layout
         collectionView.collectionViewLayout = configureLayout()
     }
-    
+
     private func configureLayout() -> UICollectionViewCompositionalLayout {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalWidth(0.5))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -98,6 +106,31 @@ extension DetailsViewController {
 
         let section = NSCollectionLayoutSection(group: group)
 
+        let footerHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50.0))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: footerHeaderSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top)
+        section.boundarySupplementaryItems = [header]
+
         return UICollectionViewCompositionalLayout(section: section)
+    }
+}
+
+class DetailsInfoHeader: UICollectionReusableView {
+    static let identifier = "DetailsInfoHeader"
+    @IBOutlet var koreanNameLabel: UILabel!
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var englishNameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func configure(_ koreanName: String, _ englishName: String, _ description: String) {
+        koreanNameLabel.text = koreanName
+        englishNameLabel.text = englishName
+        descriptionLabel.text = description
     }
 }
